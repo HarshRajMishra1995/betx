1960f6f5cb5c2c0df4e484e96c86062dcd35de4c
unknown <HarshRajMishra.1995@gmail.com>
2023-06-08 09:54:03 +0530

    completed till bookmaker market

--- betx/src/component/Match/index.js
+++ betx/src/component/Match/index.js
@@ -59,6 +59,20 @@ function index() {
 						</tr>
 					</tbody>
 				</table>
+				<ul className="match-btn">
+					<li>
+						<a style={{ cursor: "pointer" }} className="btn-pin">
+							<i className="bi bi-pin-fill"></i>
+							<p>Pin</p>
+						</a>
+					</li>
+					<li>
+						<a style={{ cursor: "pointer" }} className="btn-refresh">
+							<i className="bi bi-arrow-clockwise"></i>
+							Refresh
+						</a>
+					</li>
+				</ul>
 				<div
 					id="naviMarket"
 					className="market-type ps ps--theme_default"
@@ -98,6 +112,211 @@ function index() {
 							style={{ top: "0px", height: "0px" }}
 						></div>
 					</div>
+				</div>
+				<div className="bets-wrap">
+					<div
+						id="minMaxBox"
+						className="limit_info-popup"
+						style={{ display: "none" }}
+					>
+						<a class="close" onclick="javascript:$j('#minMaxBox').fadeOut();">
+							Close
+						</a>
+						<dl>
+							<dt id="minMaxDt">Min / Max</dt>
+							<dt id="maxDt" style={{ display: "none" }}>
+								Max
+							</dt>
+							<dd id="minMaxInfo">100 / 12500</dd>
+						</dl>
+					</div>
+					<a
+						id="minMaxButton"
+						onclick="javascript:$j('#minMaxBox').fadeIn().css('display', 'flex');"
+						className="bet-limit"
+					></a>
+					<dl id="betsHead" className="bets-selections-head">
+						<dt>
+							<a className="a-depth" id="marketDepthBtn">
+								Markets Depth
+							</a>
+							{/* <p>
+								<span>Matched</span>
+								<strong id="totalMatched">PIN 0</strong>
+							</p> */}
+						</dt>
+						<dd className="mode-land"></dd>
+						<dd className="mode-land"></dd>
+						<dd>Back</dd>
+						<dd>Lay</dd>
+						<dd className="mode-land"></dd>
+						<dd className="mode-land"></dd>
+					</dl>
+					<dl className="bets-selections" style={{ display: "flex" }}>
+						<dt>
+							<div
+								id="horseInfo"
+								class="horse-info"
+								style={{ display: "none" }}
+							>
+								<ul id="horseBox" class="horse-box">
+									<li id="clothNumberAlpha"></li>
+									<li id="stallDraw"></li>
+								</ul>
+								<div id="uniform" class="uniform">
+									<img src="" />
+								</div>
+								<ul id="horseName" class="horse-name">
+									<li id="runnerName"></li>
+									<li id="jockyName"></li>
+								</ul>
+							</div>
+							<h4>Hyderabad T10</h4>
+						</dt>
+						{/* <dd id="back_3" className="mode-land">
+							<a className="back-3">
+								<span></span>
+							</a>
+						</dd>
+						<dd id="back_2" className="mode-land">
+							<a className="back-2">
+								<span></span>
+							</a>
+						</dd> */}
+						<dd id="back_1">
+							<a className="back-1">
+								<strong>--</strong>
+								<span>--</span>
+							</a>
+						</dd>
+						<dd id="lay_1">
+							<a className="lay-1">
+								<span>
+									<strong>--</strong>
+									<span>--</span>
+								</span>
+							</a>
+						</dd>
+						{/* <dd id="lay_2" className="mode-land">
+							<a className="lay-2">
+								<span></span>
+							</a>
+						</dd>
+						<dd id="lay_3" className="mode-land">
+							<a className="lay-3">
+								<span></span>
+							</a>
+						</dd> */}
+					</dl>
+					<dl className="bets-selections" style={{ display: "flex" }}>
+						<dt>
+							<div
+								id="horseInfo"
+								class="horse-info"
+								style={{ display: "none" }}
+							>
+								<ul id="horseBox" class="horse-box">
+									<li id="clothNumberAlpha"></li>
+									<li id="stallDraw"></li>
+								</ul>
+								<div id="uniform" class="uniform">
+									<img src="" />
+								</div>
+								<ul id="horseName" class="horse-name">
+									<li id="runnerName"></li>
+									<li id="jockyName"></li>
+								</ul>
+							</div>
+							<h4>Rajasthan T10</h4>
+						</dt>
+						{/* <dd id="back_3" className="mode-land">
+							<a className="back-3">
+								<span></span>
+							</a>
+						</dd>
+						<dd id="back_2" className="mode-land">
+							<a className="back-2">
+								<span></span>
+							</a>
+						</dd> */}
+						<dd id="back_1">
+							<a className="back-1">
+								<strong>--</strong>
+								<span>--</span>
+							</a>
+						</dd>
+						<dd id="lay_1">
+							<a className="lay-1">
+								<span>
+									<strong>--</strong>
+									<span>--</span>
+								</span>
+							</a>
+						</dd>
+						{/* <dd id="lay_2" className="mode-land">
+							<a className="lay-2">
+								<span></span>
+							</a>
+						</dd>
+						<dd id="lay_3" className="mode-land">
+							<a className="lay-3">
+								<span></span>
+							</a>
+						</dd> */}
+					</dl>
+					<div id="bookMakerWrap">
+						<div id="bookMakerTempTable" style={{ display: "none" }}></div>
+						<div
+							id="bookMakerMarketTemplate"
+							className="bets-wrap bets-bookmaker"
+							style={{ display: "block" }}
+						>
+							<h4>
+								<span>Bookmaker Market</span>
+								<p>| Zero Commission</p>
+								<a className="btn-fancy_info"></a>
+							</h4>
+						</div>
+					</div>
+					<dl id="betsHead" className="bets-selections-head">
+						<dt>
+							{/* <a className="a-depth" id="marketDepthBtn">
+								Markets Depth
+							</a> */}
+							{/* <p>
+								<span>Matched</span>
+								<strong id="totalMatched">PIN 0</strong>
+							</p> */}
+						</dt>
+						<dd className="mode-land"></dd>
+						<dd className="mode-land"></dd>
+						<dd>Back</dd>
+						<dd>Lay</dd>
+						<dd className="mode-land"></dd>
+						<dd className="mode-land"></dd>
+					</dl>
+					<dl className="bets-selections" style={{ display: "flex" }}>
+						<dt>
+							<div
+								id="horseInfo"
+								class="horse-info"
+								style={{ display: "none" }}
+							>
+								<ul id="horseBox" class="horse-box">
+									<li id="clothNumberAlpha"></li>
+									<li id="stallDraw"></li>
+								</ul>
+								<div id="uniform" class="uniform">
+									<img src="" />
+								</div>
+								<ul id="horseName" class="horse-name">
+									<li id="runnerName"></li>
+									<li id="jockyName"></li>
+								</ul>
+							</div>
+							<h4>Rajasthan T10</h4>
+						</dt>
+					</dl>
 				</div>
 			</div>
 		</>
